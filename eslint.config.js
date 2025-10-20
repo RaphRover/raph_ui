2e1d82bc1348e4e0da5c8ee05f42b1ac6344c625
Adrian Krzemiński <adrian@fictionlab.pl>
2025-10-20 10:47:27 +0200

    Reformat & fix workflow

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,36 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import pluginReact from "eslint-plugin-react";
-import reactRefresh from "eslint-plugin-react-refresh";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import pluginReact from 'eslint-plugin-react'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(["dist"]),
-  {
-    files: ["**/*.{ts,tsx}"],
-    plugins: { react: pluginReact, "react-hooks": reactHooks },
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      pluginReact.configs.flat.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-      eslintConfigPrettier,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        plugins: { react: pluginReact, reactHooks },
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            pluginReact.configs.flat.recommended,
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
+            eslintConfigPrettier,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+        },
     },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-  },
-]);
+])
