93a475290ff5d82bf42f6473c609ff752e135397
Adrian Krzemiński <adrian@fictionlab.pl>
2025-10-22 14:13:28 +0200

    feat: Add ROS status to navbar (#3)
    * Add `svgr` plugin to vite
    
    * Add `ROSStatus` component
    
    * Reformat

---
+++ src/components/ROSStatus/index.tsx
@@ -0,0 +1,25 @@
+import { useROSContext } from '@scripts/context/ROSContext';
+import Frame from '@components/ui/Frame';
+import WifiOn from './wifi_on.svg?react';
+import WifiOff from './wifi_off.svg?react';
+import styles from './styles.module.css';
+import clsx from 'clsx';
+
+export default function ROSStatus() {
+  const { ros } = useROSContext();
+
+  const icon = () => {
+    return ros ? (
+      <WifiOn className={clsx(styles.icon, styles.green)} />
+    ) : (
+      <WifiOff className={clsx(styles.icon, styles.red)} />
+    );
+  };
+
+  return (
+    <Frame className={styles.frame}>
+      ROS Status
+      {icon()}
+    </Frame>
+  );
+}
