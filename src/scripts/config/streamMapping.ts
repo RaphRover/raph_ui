12e1152957e648746efa2bb4da586d075cd17b1b
Adrian Krzemiński <adrian@fictionlab.pl>
2025-10-27 16:02:04 +0100

    Add stream mappings

--- src/scripts/config/streamMapping.ts
+++ src/scripts/config/streamMapping.ts
@@ -3,4 +3,17 @@ export interface StreamNameMapping {
   displayName: string;
 }
 
-export const NAME_MAPPINGS: StreamNameMapping[] = [];
+export const NAME_MAPPINGS: StreamNameMapping[] = [
+  {
+    topicName: "/oak/left/image_raw/compressed",
+    displayName: 'OAK-D Left Stereo camera'
+  },
+  {
+    topicName: "/oak/right/image_raw/compressed",
+    displayName: 'OAK-D Right Stereo camera'
+  },
+  {
+    topicName: "/oak/rgb/image_raw/compressed",
+    displayName: 'OAK-D RGB camera'
+  },
+];
