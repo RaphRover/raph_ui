d96dc219144c65ba6c03e740e20c53348642b5ff
Adrian Krzemiński <adrian@fictionlab.pl>
2025-10-27 13:53:09 +0100

    Add Ros Streams

---
+++ src/scripts/config/streamMapping.ts
@@ -0,0 +1,6 @@
+export interface StreamNameMapping {
+  topicName: string;
+  displayName: string;
+}
+
+export const NAME_MAPPINGS: StreamNameMapping[] = [];
--- src/scripts/context/ROSContext.ts
+++ src/scripts/context/ROSContext.ts
@@ -2,11 +2,13 @@ import { createContext, useContext } from 'react';
 import { Ros } from 'roslib';
 import type { TopicManager } from '@scripts/hooks/useRosTopicManager';
 import type { RosTopic } from '@scripts/hooks/useRosTopicList';
+import type { StreamTopic } from '@scripts/hooks/useRosStreamList';
 
 interface ROSContextProps {
   ros: Ros | null;
   topicManager: TopicManager;
   topicList: RosTopic[];
+  streamList: StreamTopic[];
 }
 
 export const ROSContext = createContext<ROSContextProps | undefined>(undefined);
