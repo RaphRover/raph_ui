add31649e6651b3e226a015d3fbacf334e9c95f2
Adrian Krzemiński <adrian@fictionlab.pl>
2025-10-29 13:11:57 +0100

    Add useRosParam

---
+++ src/scripts/hooks/useRosParam.ts
@@ -0,0 +1,158 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { Param, Ros } from 'roslib';
+
+export interface RosParam<T> {
+  value: T | null;
+  get: () => Promise<T>;
+  set: (value: T) => Promise<T>;
+}
+
+type RosParamType = {
+  int: number;
+  float: number;
+  string: string;
+  bool: boolean;
+  'int[]': number[];
+  'float[]': number[];
+  'string[]': string[];
+  'bool[]': boolean[];
+};
+
+export default function useRosParam<K extends keyof RosParamType>(
+  ros: Ros | null,
+  paramNode: string,
+  paramName: string,
+  timeout: number,
+  paramType: K,
+): RosParam<RosParamType[K]> {
+  type valueType = RosParamType[K];
+
+  const [paramValue, setParamValue] = useState<valueType | null>(null);
+  const paramRef = useRef<Param | null>(null);
+  const isCallingRef = useRef<boolean>(false);
+
+  const getParam = useCallback(async () => {
+    if (!paramRef.current) {
+      return Promise.reject(new Error('Param not initialized.'));
+    }
+
+    if (!isCallingRef.current) {
+      const errorMessage = `Param call to ${paramName} is already in progress.`;
+      console.warn('[useRosParam] ' + errorMessage);
+      return Promise.reject(new Error(errorMessage));
+    }
+
+    isCallingRef.current = true;
+
+    const param = paramRef.current;
+
+    const paramGetPromise = new Promise<valueType>((resolve, reject) => {
+      param.get(
+        (response: valueType) => {
+          resolve(response);
+        },
+        (error: string) => {
+          reject(new Error(error));
+        },
+      );
+    });
+
+    const timeoutPromise = new Promise<valueType>((_, reject) => {
+      setTimeout(() => {
+        reject(
+          new Error(`Get call to ${paramName} timed out after ${timeout} ms.`),
+        );
+      }, timeout);
+    });
+
+    try {
+      return Promise.race([paramGetPromise, timeoutPromise]);
+    } finally {
+      isCallingRef.current = false;
+    }
+  }, [paramName, timeout]);
+
+  const setRosParam = useCallback(
+    (value: valueType) => {
+      if (!paramRef.current) {
+        return Promise.reject(new Error('Param not initialized.'));
+      }
+
+      if (!isCallingRef.current) {
+        const errorMessage = `Param call to ${paramName} is already in progress.`;
+        console.warn('[useRosParam] ' + errorMessage);
+        return Promise.reject(new Error(errorMessage));
+      }
+
+      isCallingRef.current = true;
+
+      const param = paramRef.current;
+
+      let parsedValue: string;
+
+      switch (paramType) {
+        case 'int':
+          if (typeof value === 'number') parsedValue = value.toFixed(0);
+          break;
+        case 'float':
+          if (typeof value === 'number') parsedValue = value.toPrecision(4);
+          break;
+        default:
+          parsedValue = value.toString();
+          break;
+      }
+
+      const paramSetPromise = new Promise<valueType>((resolve, reject) => {
+        param.set(
+          parsedValue,
+          (response: valueType) => {
+            setParamValue(response);
+            resolve(response);
+          },
+          (error: string) => {
+            reject(new Error(error));
+          },
+        );
+      });
+
+      const timeoutPromise = new Promise<valueType>((_, reject) => {
+        setTimeout(() => {
+          reject(
+            new Error(
+              `Set call to ${paramName} timed out after ${timeout} ms.`,
+            ),
+          );
+        }, timeout);
+      });
+
+      return Promise.race([paramSetPromise, timeoutPromise]).finally(() => {
+        isCallingRef.current = false;
+      });
+    },
+
+    [paramName, paramType, timeout],
+  );
+
+  useEffect(() => {
+    if (!ros) {
+      return;
+    }
+    if (!paramRef.current) {
+      paramRef.current = new Param({
+        ros: ros,
+        name: paramNode + ':' + paramName,
+      });
+      console.debug(`[useRosParam] ROS param ${paramName} initialized`);
+    }
+    getParam();
+    return () => {
+      paramRef.current = null;
+      setParamValue(null);
+      console.debug(`[useRosParam] ROS param ${paramName} uninitialized`);
+    };
+  }, [getParam, paramName, paramNode, ros]);
+
+  // TODO in future - add subscription to /parameter_events topics with param-specific callback
+
+  return { value: paramValue, set: setRosParam, get: getParam };
+}
