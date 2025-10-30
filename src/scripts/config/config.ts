5ca01047ae426b6f07207da8ae856e0f1d8a7c1d
Adrian Krzemiński <adrian@fictionlab.pl>
2025-10-30 12:32:18 +0100

    feat: Add hooks for Ros Services and Params (#10)
    * Add `useRosService`
    
    * Block concurrent service calls, add isLoading state
    
    * Reformat
    
    * Delete accidentally added file
    
    * Add useRosParam
    
    * Unify get and set Param functions
    
    * Reformat
    
    * Clear timer & convert callService to async function
    
    * Add `FLOAT_PRECISION` config
    
    * Prevent useEffect fire on getParam change
    
    * Reformat
    
    * Move param float precision to ROS config & add JSDoc to `config.ts`
    
    * Add missing types
    
    * Move floatPrecision declaration

--- src/scripts/config/config.ts
+++ src/scripts/config/config.ts
@@ -1,20 +1,68 @@
-export const ROS_CONFIG = {
+interface RosConfig {
+  /** Robot hostname that UI will connect to (from .env or location.hostname) */
+  HOSTNAME: string;
+  /** WebSocket port for ROS connection */
+  PORT: number;
+  /** Interval (ms) between reconnection attempts */
+  RECONNECT_INTERVAL_MS: number;
+  /** Interval for polling ROS topics (ms) */
+  TOPIC_POLL_INTERVAL_MS: number;
+  /** Precision of setting float ROS parameters */
+  PARAM_FLOAT_PRECISION: number;
+}
+
+/**
+ * Configuration for ROS connection.
+ * @type {RosConfig}
+ */
+export const ROS_CONFIG: RosConfig = {
   HOSTNAME: import.meta.env.VITE_ROBOT_HOSTNAME || location.hostname,
   PORT: 9090,
   RECONNECT_INTERVAL_MS: 5000,
   TOPIC_POLL_INTERVAL_MS: 5000,
+  PARAM_FLOAT_PRECISION: 4,
 };
 
+/**
+ * Default stream name used on first load of the UI.
+ * @type {string}
+ */
 export const DEFAULT_STREAM_NAME = '/oak/rgb/image_raw/compressed';
 
-export const BATTERY_CONFIG = {
+interface BatteryConfig {
+  /** Battery status refresh interval in milliseconds */
+  REFRESH_INTERVAL_MS: number;
+  /** Decimal precision for battery percentage display */
+  DISPLAY_PRECISION: number;
+  /** Upper battery percentage considered critical */
+  CRITICAL_LEVEL_PERCENT: number;
+  /** Upper battery percentage considered warning */
+  WARNING_LEVEL_PERCENT: number;
+}
+
+/**
+ * Configuration for battery status display.
+ * @type {BatteryConfig}
+ */
+export const BATTERY_CONFIG: BatteryConfig = {
   REFRESH_INTERVAL_MS: 1000,
   DISPLAY_PRECISION: 2,
   CRITICAL_LEVEL_PERCENT: 20,
   WARNING_LEVEL_PERCENT: 60,
 };
 
-export const IMU_CONFIG = {
+interface ImuConfig {
+  /** IMU status refresh interval in milliseconds */
+  REFRESH_INTERVAL_MS: number;
+  /** Decimal precision for IMU data display */
+  DISPLAY_PRECISION: number;
+}
+
+/**
+ * Configuration for IMU status display.
+ * @type {ImuConfig}
+ */
+export const IMU_CONFIG: ImuConfig = {
   REFRESH_INTERVAL_MS: 1000,
   DISPLAY_PRECISION: 2,
 };
---
+++ src/scripts/hooks/useRosService.ts
@@ -0,0 +1,96 @@
+import { useROSContext } from '@scripts/context/ROSContext';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { Service } from 'roslib';
+
+/**
+ * @param serviceName Service name string
+ * @param serviceType Service type string
+ * @param timeout Service call timeout, defaults to `5000` ms
+ * @returns Promise
+ */
+export default function useRosService<Request, Response>(
+  serviceName: string,
+  serviceType: string,
+  timeout: number = 5000,
+) {
+  const { ros } = useROSContext();
+
+  const serviceRef = useRef<Service<Request, Response> | null>(null);
+  const isCallingRef = useRef<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  // Service initialization & clean up
+  useEffect(() => {
+    if (!ros) {
+      console.debug(`[useRosService] Service ${serviceName} uninitialized`);
+      serviceRef.current = null;
+      return;
+    }
+
+    const service = new Service<Request, Response>({
+      ros,
+      name: serviceName,
+      serviceType,
+    });
+    serviceRef.current = service;
+    console.debug(`[useRosService] Service ${serviceName} initialized`);
+
+    return () => {
+      console.debug(`[useRosService] Service ${serviceName} uninitialized`);
+      serviceRef.current = null;
+    };
+  }, [ros, serviceName, serviceType]);
+
+  const callService = useCallback(
+    async (request: Request): Promise<Response> => {
+      if (isCallingRef.current) {
+        const errorMessage = `Service call to ${serviceName} is already in progress.`;
+        console.warn('[useRosService] ' + errorMessage);
+        return Promise.reject(new Error(errorMessage));
+      }
+
+      if (!serviceRef.current) {
+        return Promise.reject(new Error('Service not initialized.'));
+      }
+
+      isCallingRef.current = true;
+      setIsLoading(true);
+
+      const service = serviceRef.current;
+
+      const serviceCallPromise = new Promise<Response>((resolve, reject) => {
+        service.callService(
+          request,
+          (response: Response) => {
+            resolve(response);
+          },
+          (error: string) => {
+            reject(new Error(error));
+          },
+        );
+      });
+
+      let timer: number | undefined;
+      const timeoutPromise = new Promise<Response>((_, reject) => {
+        timer = setTimeout(() => {
+          reject(
+            new Error(
+              `Service call to ${serviceName} timed out after ${timeout}ms.`,
+            ),
+          );
+        }, timeout);
+      });
+
+      try {
+        return await Promise.race([serviceCallPromise, timeoutPromise]);
+      } finally {
+        isCallingRef.current = false;
+        setIsLoading(false);
+        clearTimeout(timer);
+      }
+    },
+    [serviceName, timeout],
+  );
+
+  return { callService, isLoading };
+}
