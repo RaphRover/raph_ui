67d90620c7e0fad7abf54ae82ad476a7f4b93d49
Adrian Krzemiński <adrian@fictionlab.pl>
2025-11-05 16:22:58 +0100

    Add useFirstRender

---
+++ src/scripts/hooks/useFirstRender.ts
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from 'react';
+
+export default function useFirstRender(callback: () => void) {
+  const ref = useRef(true);
+
+  useEffect(() => {
+    const isFirstRender = ref.current;
+    if (isFirstRender) callback();
+    ref.current = false;
+  });
+}
