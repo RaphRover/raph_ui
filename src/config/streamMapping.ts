68f74cc227a82904e9a4c77474f8698f971583ec
Jan Hernas <40989249+jasiex01@users.noreply.github.com>
2026-03-03 12:08:49 +0100

    chore: Update topic mappings for new oak implementation (#24)
    * feat: Update topic mappings for new OAK camera streams
    
    * fix: Improve topic filtering for compressed image streams

--- src/config/streamMapping.ts
+++ src/config/streamMapping.ts
@@ -9,11 +9,23 @@ export const NAME_MAPPINGS: StreamNameMapping[] = [
     displayName: 'OAK-D Left Stereo camera',
   },
   {
+    topicName: '/oak/left_rect/image_rect/compressed',
+    displayName: 'OAK-D Left Stereo camera (rectified)',
+  },
+  {
     topicName: '/oak/right/image_raw/compressed',
     displayName: 'OAK-D Right Stereo camera',
   },
   {
+    topicName: '/oak/right_rect/image_rect/compressed',
+    displayName: 'OAK-D Right Stereo camera (rectified)',
+  },
+  {
     topicName: '/oak/rgb/image_raw/compressed',
     displayName: 'OAK-D RGB camera',
+  },
+  {
+    topicName: '/oak/rgb_rect/image_rect/compressed',
+    displayName: 'OAK-D RGB camera (rectified)',
   },
 ];
